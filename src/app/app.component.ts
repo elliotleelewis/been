f7e7a80e73aeee96f5641262eb7c5df4d636e909
Elliot Lewis <elliotleelewis@gmail.com>
2020-12-23 22:46:47 +0000

    feat: added basic page layout (#14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-	minZoom = 1;
-}
+export class AppComponent {}
