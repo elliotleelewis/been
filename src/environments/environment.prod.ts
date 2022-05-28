06dcea3b38cd612a98dfef756833c25b1df19c38
Elliot Lewis <elliotleelewis@gmail.com>
2022-05-27 20:28:30 -0400

    chore: added `stylelint-use-logical-spec` plugin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import { Environment } from '../app/models/environment';
+import type { Environment } from '../app/models/environment';
 
 export const ENVIRONMENT: Environment = {
 	production: true,
-	apiKeyMapbox: process.env.API_KEY_MAPBOX ?? '',
+	apiKeyMapbox: process.env['API_KEY_MAPBOX'] ?? '',
 };
