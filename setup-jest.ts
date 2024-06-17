baebf0b6cecfe0b661e8d89bbdb27c3568d9183f
Elliot Lewis <17044667+elliotleelewis@users.noreply.github.com>
2024-06-17 09:27:08 -0700

    chore: updated ESLint runner

--- setup-jest.ts
+++ setup-jest.ts
@@ -10,13 +10,15 @@ global.TextDecoder = TextDecoder as typeof global.TextDecoder;
  * Monkey patch console warn and error to fail if a test makes calls to console.warn or console.error.
  */
 console.warn = (message?: unknown, ...optionalParams: unknown[]): void => {
-	const params = optionalParams ? `\nParams: ${String(optionalParams)}` : '';
+	const params =
+		optionalParams.length > 0 ? `\nParams: ${String(optionalParams)}` : '';
 	throw new Error(
 		`Test contained console warning:\n${String(message)}${params}`,
 	);
 };
 console.error = (message?: unknown, ...optionalParams: unknown[]): void => {
-	const params = optionalParams ? `\nParams: ${String(optionalParams)}` : '';
+	const params =
+		optionalParams.length > 0 ? `\nParams: ${String(optionalParams)}` : '';
 	throw new Error(
 		`Test contained console error:\n${String(message)}${params}`,
 	);
