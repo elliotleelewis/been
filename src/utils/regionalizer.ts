c23998af2a3158281767cc91b55c60314c9f5af3
Elliot Lewis <elliotleelewis@gmail.com>
2024-09-05 21:46:28 -0700

    fix: region sorting

--- src/utils/regionalizer.ts
+++ src/utils/regionalizer.ts
@@ -2,12 +2,27 @@ import { type Country } from '../models/country';
 import { type Region } from '../models/region';
 
 export const regionalizer = (arr: readonly Country[]): readonly Region[] =>
-	arr.reduce((prev: Region[], current) => {
-		const item = prev.find((i) => i.name === current.region);
-		if (item) {
-			item.values.push(current);
-		} else {
-			prev.push({ name: current.region, values: [current] });
-		}
-		return prev;
-	}, []);
+	arr
+		.reduce((prev: Region[], current) => {
+			const item = prev.find((i) => i.name === current.region);
+			if (item) {
+				item.values.push(current);
+			} else {
+				prev.push({ name: current.region, values: [current] });
+			}
+			return prev;
+		}, [])
+		.sort(({ name: aName }, { name: bName }) => {
+			if (aName === '' && bName === '') {
+				return 0;
+			}
+
+			if (aName === '') {
+				return 1;
+			}
+			if (bName === '') {
+				return -1;
+			}
+
+			return aName.localeCompare(bName);
+		});
