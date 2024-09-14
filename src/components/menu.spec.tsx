f74b9f264908372b2460093df348b86ee8a920fa
Elliot Lewis <17044667+elliotleelewis@users.noreply.github.com>
2024-09-14 11:27:18 -0700

    test: removed extra unit test

--- src/components/menu.spec.tsx
+++ src/components/menu.spec.tsx
@@ -7,15 +7,31 @@ import {
 	it,
 	vi,
 } from 'vitest';
-import { useCountries } from '../contexts/countries-context';
 import { Menu } from './menu';
 
 describe('Menu', () => {
 	beforeEach(() => {
 		vi.mock(import('../contexts/countries-context'), () => ({
 			useCountries: vi.fn(() => ({
-				countries: [],
-				regions: [],
+				countries: [
+					{
+						name: 'United Kingdom',
+						iso3166: 'GB',
+						region: 'Europe',
+					},
+				],
+				regions: [
+					{
+						name: 'Europe',
+						values: [
+							{
+								name: 'United Kingdom',
+								iso3166: 'GB',
+								region: 'Europe',
+							},
+						],
+					},
+				],
 				focus: null,
 				addCountry: vi.fn(),
 				removeCountry: vi.fn(),
@@ -25,33 +41,6 @@ describe('Menu', () => {
 	});
 
 	it('should render', () => {
-		const result = render(<Menu header={<div>Header!</div>} />);
-
-		expect(result.asFragment()).toMatchSnapshot();
-	});
-
-	it('should render with a country', () => {
-		vi.mocked(useCountries, { partial: true }).mockReturnValue({
-			countries: [
-				{
-					name: 'United Kingdom',
-					iso3166: 'GB',
-					region: 'Europe',
-				},
-			],
-			regions: [
-				{
-					name: 'Europe',
-					values: [
-						{
-							name: 'United Kingdom',
-							iso3166: 'GB',
-							region: 'Europe',
-						},
-					],
-				},
-			],
-		});
 		const result = render(<Menu header={<div>Header!</div>} />);
 
 		expect(result.asFragment()).toMatchSnapshot();
