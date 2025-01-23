833c0bebf88d729227e9e2354cd85c241b571e39
Elliot Lewis <elliotleelewis@gmail.com>
2025-01-23 04:57:35 +0000

    chore: fixed ordering

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // biome-ignore lint/style/noDefaultExport: Required structure for Vite
