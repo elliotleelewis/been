f0f3ee486207eb47bba6364033498dbdd6b3bf26
Elliot Lewis <17044667+elliotleelewis@users.noreply.github.com>
2026-07-26 01:33:53 -0700

    fix: tick checkboxes when they are clicked (#816)

--- src/components/menu.spec.tsx
+++ src/components/menu.spec.tsx
@@ -1,6 +1,6 @@
-import { render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { Provider } from "jotai";
-import { describe, expect, it, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import type { Country } from "../models/country.ts";
 import { rawCountriesAtom } from "../state/atoms.ts";
 import { HydrateAtoms } from "../utils/test.ts";
@@ -12,16 +12,49 @@ const country: Country = {
 	region: "Europe",
 };
 
+const renderMenu = (): ReturnType<typeof render> =>
+	render(
+		<Provider>
+			<HydrateAtoms initialValues={[[rawCountriesAtom, { [country.iso3166]: country }]]}>
+				<Menu fullscreen={false} toggleFullscreen={vi.fn<() => void>()} />
+			</HydrateAtoms>
+		</Provider>,
+	);
+
 describe("menu", () => {
+	beforeEach(() => {
+		window.localStorage.clear();
+	});
+
+	// Auto-cleanup is not registered as the suite does not run with `globals`.
+	afterEach(() => {
+		cleanup();
+	});
+
 	it("should render", () => {
-		const result = render(
-			<Provider>
-				<HydrateAtoms initialValues={[[rawCountriesAtom, { [country.iso3166]: country }]]}>
-					<Menu fullscreen={false} toggleFullscreen={vi.fn<() => void>()} />
-				</HydrateAtoms>
-			</Provider>,
-		);
+		const result = renderMenu();
 
 		expect(result.asFragment()).toMatchSnapshot();
+	}, 5000);
+
+	it("should tick the checkbox when it is clicked", () => {
+		const result = renderMenu();
+		const checkbox = result.getByLabelText("Visited");
+
+		expect(checkbox).not.toBeChecked();
+
+		checkbox.click();
+
+		expect(checkbox).toBeChecked();
+	}, 5000);
+
+	it("should untick the checkbox when it is clicked again", () => {
+		const result = renderMenu();
+		const checkbox = result.getByLabelText("Visited");
+
+		checkbox.click();
+		checkbox.click();
+
+		expect(checkbox).not.toBeChecked();
 	}, 5000);
 });
